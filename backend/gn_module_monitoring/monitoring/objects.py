ac8cfc7f0071909eb4032288a1ed7591c85f92b2
andriacap <111564663+andriacap@users.noreply.github.com>
2023-04-26 10:20:31 +0200

    Feat/edit site (#56)
    * feat: edit site (init value btn-select)
    
    - Change button select to accept incoming intial values
    - Change visit component to add btn-select and form-g inside html
    
    - WIP: update form component when initial values is up to date
    
    Reviewed-by: andriac
    [Refs_ticket] :  #5 , #6
    
    * feat: get information when edit site
    
    - Get all fields from specific site.json into editform
    - Fix problem redirection if edit object site
    
    - WIP : check how to update specific fields from object
    - WIP : check how to manage listOption types site when reload or when
      come back into component after first init
    
    Reviewed-by: andriac
    [Refs_ticket]: #5 , #6
    
    * fix: forgot add apiService property binding
    
    Reviewed-by:andriac
    
    * feat: update site with good properties
    
    Change types_site object to array of ids
    Remove extra key "dataComplement" to assign config
    
    Reviewed-by:andriac
    [Refs_ticket]: #5 , #6
    
    * feat: prevent form appear if no type-site
    
    - Hide form if btn-list type-site not selected
    - Add custom error message mat-error if not selected (directive + custom
      message)
    
    WIP: error message is showing up only if not selected after touched .
    Maybe need to use asyncValidator ?
    
    Reviewed-by: andriac
    [Refs_tickets]: #5 , #6 , #54
    
    * feat: change order to emit event bEdit
    
    - Change order between change formService.changeData and bEditChange
    - Preprocess for types_site seems to be useless (see todo)
    
    Reviewed-by: andriac
    [Refs_ticket]: : #5 , #6 , #54
    
    * feat: solve request changes
    
    - Apply prettier
    - Change object key [''] by type properties
    - Remove unused preprocess_data lines
    
    [Refs_ticket] : closes #54

--- backend/gn_module_monitoring/monitoring/objects.py
+++ backend/gn_module_monitoring/monitoring/objects.py
@@ -4,13 +4,12 @@ from geonature.utils.env import DB
 from geonature.core.gn_commons.models import TModules
 
 class MonitoringModule(MonitoringObject):
-
     def get(self, param_value=None, param_name=None, depth=0):
-        '''
-            pour récupérer le module sans l'id_module mais avec le module_code
-        '''
+        """
+        pour récupérer le module sans l'id_module mais avec le module_code
+        """
         if not param_name:
-            param_name = 'module_code'
+            param_name = "module_code"
             if not param_value:
                 param_value = self._module_code
         MonitoringObject.get(self, param_value, param_name, depth)
@@ -19,19 +18,15 @@ class MonitoringModule(MonitoringObject):
 
 
 class MonitoringSite(MonitoringObjectGeom):
-    '''
-        PATCH
-        pour pouvoir renseigner la table cor_site_module
-        avec la méthode from_dict
-    '''
+    """
+    PATCH
+    pour pouvoir renseigner la table cor_site_module
+    avec la méthode from_dict
+    """
 
     def preprocess_data(self, data):
-        type_site_ids = [type_site.id_nomenclature_type_site for type_site in self._model.types_site]
-        if len(data['types_site']) >0 :
-            for id_type_site in data['types_site']:
-                if int(id_type_site) not in type_site_ids:
-                    type_site_ids.append(id_type_site)
-            #TODO: A enlever une fois qu'on aura enelever le champ "id_nomenclature_type_site" du model et de la bdd
-            data["id_nomenclature_type_site"]=data["types_site"][0]
-
-        data['types_site'] = type_site_ids
+        if len(data["types_site"]) > 0 and all(isinstance(x, int) for x in data["types_site"]):
+            data["id_nomenclature_type_site"] = data["types_site"][0]
+        else:
+            data["id_nomenclature_type_site"] = data["types_site"][0]["id_nomenclature_type_site"]
+            # TODO: A enlever une fois qu'on aura enelever le champ "id_nomenclature_type_site" du model et de la bdd
